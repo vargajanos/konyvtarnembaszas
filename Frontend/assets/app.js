bda5320e2c20a07a67bc26e1b5dca2929c2aedb2
Csoki <csokaandras@turr.hu>
2024-10-16 12:23:29 +0200

    Szerzők műveletek kész

--- Frontend/assets/app.js
+++ Frontend/assets/app.js
@@ -1,4 +1,6 @@
 var xhr = new XMLHttpRequest();
+var authors
+var loadedAuthor
 
 render('author')
 async function render(view){
@@ -24,16 +26,16 @@ function getAuthors(){
     xhr.send();
     xhr.onreadystatechange = function(){
         if (xhr.readyState == 4 && xhr.status == 200) {
-            var data = JSON.parse(xhr.response)
-            data.forEach((item,index) => {
+            authors = JSON.parse(xhr.response)
+            authors.forEach((item,index) => {
                 tbody.innerHTML +=
                 `<tr>
                     <th>${index+1}</th>
                     <td>${item.name}</td>
                     <td class="text-center">${item.birth.split('T')[0]}</td>
                     <td class="text-end">
-                        <button type="button" class="btn gonb" >Módosítás</button>
-                        <button type="button" class="btn gonb" >Törlés</button>
+                        <button type="button" class="btn gonb mb-1" onclick="loadEdit(${item.ID})">Módosítás</button>
+                        <button type="button" class="btn gonb mb-1" onclick="deleteAuthor(${item.ID})">Törlés</button>
                     </td>
                 </tr>`
             });
@@ -41,7 +43,7 @@ function getAuthors(){
     }
 }
 
-function addAuthor(){    
+function addAuthor(){
     var data = JSON.stringify({
         name: document.querySelector("#name").value,
         birth: document.querySelector("#birth").value
@@ -57,6 +59,72 @@ function addAuthor(){
             getAuthors()
             document.querySelector("#name").value = null
             document.querySelector("#birth").value = null
+            return
+        }
+        if(xhr.readyState == 4 && xhr.status != 200){
+            alert(xhr.response)
+            return
+        }
+    }
+}
+
+function loadEdit(id){
+    let author = authors.find((item) => item.ID == id)
+
+    if(loadedAuthor == author){
+        loadedAuthor = null
+        document.querySelector("#name").value = null
+        document.querySelector("#birth").value = null
+
+        document.querySelector("#addAuthor").classList.remove("d-none")
+        document.querySelector("#editAuthor").classList.add("d-none")
+    }
+    else{
+        loadedAuthor = author
+        document.querySelector("#name").value = author.name
+        document.querySelector("#birth").value = author.birth.split('T')[0]
+
+        document.querySelector("#addAuthor").classList.add("d-none")
+        document.querySelector("#editAuthor").classList.remove("d-none")
+    }
+}
+
+function editAuthor(){
+    var data = JSON.stringify({
+        name: document.querySelector("#name").value,
+        birth: document.querySelector("#birth").value
+    })
+    
+    xhr.open("PATCH", `http://localhost:3000/authors/${loadedAuthor.ID}`, true)
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.send(data)
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status == 200){
+            alert("Sikeres módosítás")
+            getAuthors()
+            document.querySelector("#name").value = null
+            document.querySelector("#birth").value = null
+            document.querySelector("#addAuthor").classList.remove("d-none")
+            document.querySelector("#editAuthor").classList.add("d-none")
+            return
+        }
+        if(xhr.readyState == 4 && xhr.status != 200){
+            alert(xhr.response)
+            return
+        }
+    }
+}
+
+function deleteAuthor(id){
+    xhr.open("DELETE", `http://localhost:3000/authors/${id}`, true)
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.send()
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status == 200){
+            alert("Sikeres törlés")
+            getAuthors()
             return
         }
         if(xhr.readyState == 4 && xhr.status != 200){
