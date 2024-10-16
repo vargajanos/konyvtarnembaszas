449efaaed8061dffb25e389e85418e3ffbff2def
Csoki <csokaandras@turr.hu>
2024-10-16 09:31:42 +0200

    Frontand alapok

---
+++ Frontend/assets/app.js
@@ -0,0 +1,26 @@
+
+
+render('author')
+async function render(view){
+    let main = document.querySelector('main');
+    main.innerHTML = await (await fetch(`views/${view}.html`)).text();
+    
+    switch(view){
+        case 'author':{
+            getAuthors()
+            break;
+        }
+        case 'book':{
+            getBooks()
+            break;
+        }
+    }
+}
+
+function getAuthors(){
+
+}
+
+function getBooks(){
+
+}
